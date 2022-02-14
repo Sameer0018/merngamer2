0e1a10f878640e5feb69369f2731b54d38823b32
Sameer Hussain <87130217+Sameer0018@users.noreply.github.com>
2022-02-14 21:32:51 +0530

    Update About.js

--- server/client/src/components/About.js
+++ server/client/src/components/About.js
@@ -13,7 +13,7 @@ const About = () => {
     const callAboutPage = async () => {
         try {
 
-            const res = await fetch('http://localhost:3000/about', {
+            const res = await fetch('https://merngamer2.herokuapp.com/about', {
                  method: "GET",
                 headers: {
                     Accept: "application/json",
